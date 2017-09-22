250a1187bd14bac7a1d4d7b84186a284a86897ea
Sara Hopper <sara.hopper@gmail.com>
2017-09-22 01:58:16 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,13 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com' do |req|
+     req.params['client_id'] = ENV['client_id']
+     req.params['client_secret'] = ENV['client_secret']
+     req.params['v'] = '20160201'
+     req.params['near'] = params[:zipcode]
+     req.params['query'] = 'coffee shop'
+   end
+   render 'search'
   end
 end
